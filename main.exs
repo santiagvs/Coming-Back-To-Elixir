4c2ae03178c7faabafe993eaa347c901078c5122
Santiago <victor.lacsilva0102@gmail.com>
2023-06-05 11:05:31 -0300

    first commit

---
+++ main.exs
@@ -0,0 +1,22 @@
+defmodule MeuModulo do
+  import IO, only: [puts: 1] # aridade
+  import Kernel, except: [inspect: 1]
+
+  alias MeuModulo.Math, as: MyMath
+
+  require Integer
+
+  def ola_mundo do
+    inspect(MyMath.soma(2, 2))
+  end
+
+  def exibe_se_e_par(numero) do
+    puts("O número #{numero} é par? #{Integer.is_even(numero)}")
+  end
+
+  def inspect(parametro) do
+    puts("Começando a inspeção")
+    puts(parametro)
+    puts("Terminando a inspeção")
+  end
+end
